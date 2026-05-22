e8b6feb3f9fec28de7a4b260e46f3ef59405a27d
rotlug <rotlug77@gmail.com>
2026-05-22 21:50:33 +0300

    feat: add game button

--- src/ui.rs
+++ src/ui.rs
@@ -1,6 +1,8 @@
 use glycin::Loader;
 use relm4::gtk::gdk;
 
+pub mod game_button;
+
 pub async fn bytes_to_texture(bytes: Vec<u8>) -> Result<gdk::Texture, glycin::ErrorCtx> {
     let image = Loader::new_vec(bytes).load().await?;
 
---
+++ src/ui/game_button.rs
@@ -0,0 +1,93 @@
+use std::sync::Arc;
+
+use adw::prelude::*;
+use relm4::{gtk::gdk, prelude::*};
+
+use crate::{game::Game, ui::bytes_to_texture};
+
+pub struct GameButton {
+    game: Arc<Game>,
+    texture: gdk::Texture,
+}
+
+#[relm4::factory(pub async)]
+impl AsyncFactoryComponent for GameButton {
+    type Input = ();
+    type Output = ();
+    type Init = Arc<Game>;
+    type ParentWidget = gtk::FlowBox;
+    type CommandOutput = ();
+
+    view! {
+        gtk::Box {
+            set_orientation: gtk::Orientation::Vertical,
+            set_valign: gtk::Align::Start,
+            set_halign: gtk::Align::Center,
+
+            adw::Clamp {
+                set_maximum_size: 200,
+
+                #[name = "cover_button"]
+                gtk::Button {
+                    set_overflow: gtk::Overflow::Hidden,
+
+                    gtk::Box {
+                        set_orientation: gtk::Orientation::Vertical,
+                        set_hexpand: false,
+                        set_vexpand: false,
+
+                        #[name = "cover"]
+                        gtk::Picture {
+                            set_content_fit: gtk::ContentFit::Cover,
+                            set_width_request: 200,
+                            set_height_request: 300,
+                        },
+
+                        #[name = "title"]
+                        gtk::Label {
+                            set_label: &self.game.metadata.as_ref().unwrap().name,
+                            set_ellipsize: gtk::pango::EllipsizeMode::End,
+                            set_hexpand: true,
+                            set_halign: gtk::Align::Start,
+                            set_margin_top: 14,
+                            set_margin_bottom: 14,
+                            set_margin_start: 12,
+                            set_margin_end: 12,
+                        }
+                    },
+
+                    set_css_classes: &["card"]
+                }
+            }
+        }
+    }
+
+    async fn init_model(
+        init: Self::Init,
+        index: &DynamicIndex,
+        sender: AsyncFactorySender<Self>,
+    ) -> Self {
+        let metadata = init.metadata.as_ref().unwrap();
+        let bytes = metadata.cover.download().await.unwrap();
+        let texture = bytes_to_texture(bytes).await.unwrap();
+
+        Self {
+            game: init,
+            texture,
+        }
+    }
+
+    fn init_widgets(
+        &mut self,
+        index: &DynamicIndex,
+        root: Self::Root,
+        returned_widget: &<Self::ParentWidget as relm4::factory::FactoryView>::ReturnedWidget,
+        sender: AsyncFactorySender<Self>,
+    ) -> Self::Widgets {
+        let widgets = view_output!();
+
+        widgets.cover.set_paintable(Some(&self.texture));
+
+        widgets
+    }
+}
